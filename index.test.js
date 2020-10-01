d44a462f2d68d625f6a98ec6235ae13510f2db74
saurav <saurav@umbrella.qa>
2020-10-01 08:31:44 +0530

    Feature: Added test function for multiply
    1. Using Test Added test function for multiply
    2. Added type: 'modules' in package.json for module export
    3. Appended .gitignore and added unchached node_modules in git
    
    see also: https://github.com/finalight/calculator/issues/14

--- index.test.js
+++ index.test.js
@@ -1,3 +1,10 @@
+const multiplyTest = require('./src/methods/multiply');
+
+
 test('that is true', () => {
     expect(true).toBe(true);
+});
+
+test('9 multiplied by 2 equals 18', () => {
+    expect(multiplyTest(2, 9)).toBe(18);
 });
