a1e7ddca68637f9f520d3182942c95eab5b2713e
Darkshredder <srivastavayash58@gmail.com>
2020-10-01 08:13:42 +0530

    added test to multiply

--- index.test.js
+++ index.test.js
@@ -1,3 +1,6 @@
-test('that is true', () => {
-    expect(true).toBe(true);
+test("that is true", () => {
+  expect(true).toBe(true);
+});
+test("multiply two numbers", () => {
+  expect(3 * 2).toBe(6);
 });
